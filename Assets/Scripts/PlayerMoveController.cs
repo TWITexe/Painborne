00b8fc8b75a540a5cc25724d0eb609cfd976f949
TWITexe <kawaii_senpaii@mail.ru>
2025-09-27 04:18:40 +0700

    Add Player move controller and ground check

---
+++ Assets/Scripts/PlayerMoveController.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMoveController : MonoBehaviour
+{
+    [SerializeField] private float speed = 4.5f;
+
+    // jump
+    [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private float groundCheckRadius = 0.2f;
+    [SerializeField] private LayerMask groundLayer;
+    private bool isGrounded;
+    private bool jumpPressed;
+
+    private Rigidbody2D rigidBody;
+    private Animator anim;
+
+    private Vector3 baseScale;
+
+    private float moveDirection;
+
+    private void Awake()
+    {
+        baseScale = transform.localScale;
+        rigidBody = GetComponent<Rigidbody2D>();
+        //anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        moveDirection = Input.GetAxisRaw("Horizontal");
+
+        //if (anim != null)
+        //    anim.SetFloat("speed", Mathf.Abs(movement.x));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            jumpPressed = true;
+
+    }
+    private void FixedUpdate()
+    {
+        Vector2 movement = new Vector2(moveDirection * speed, rigidBody.linearVelocity.y);
+        rigidBody.linearVelocity = movement;
+
+        Flip();
+        Jump();
+    }
+
+    private void Jump()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (jumpPressed && isGrounded)
+        {
+            rigidBody.linearVelocity = new Vector2(rigidBody.linearVelocity.x, jumpForce);
+        }
+
+        jumpPressed = false;
+    }
+    private void Flip()
+    {
+        if (moveDirection != 0)
+            transform.localScale = new Vector3(Mathf.Sign(moveDirection) * baseScale.x, baseScale.y, baseScale.z);
+    }
+}
