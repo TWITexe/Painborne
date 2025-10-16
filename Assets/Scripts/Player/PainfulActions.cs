d8ca3c6f24bf513d409c20663da01a9e66735a21
TWITexe <pkotelnikov2000@gmail.com>
2025-10-17 05:29:21 +0700

    feat(vfx): connect damage flash effect to Health.OnDamaged event

--- Assets/Scripts/Player/PainfulActions.cs
+++ Assets/Scripts/Player/PainfulActions.cs
@@ -1,11 +1,23 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class PainfulActions : MonoBehaviour
 {
     private Health playerHealth;
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Color flashColor = new Color(1f, 0.2f, 0.2f);
+    [SerializeField] private float flashDuration = 0.1f;
+    [SerializeField] private int flashCount = 2;
+    private Color originalColor;
+
+    private Material originalMaterial;
     void Awake()
     {
         playerHealth = gameObject.GetComponent<Health>();
+        if (spriteRenderer == null)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        originalColor = spriteRenderer.material.color;
+
     }
 
     void Update()
@@ -14,4 +26,40 @@ public class PainfulActions : MonoBehaviour
             playerHealth.TakeDamage(5);
 
     }
+    
+
+    private void OnEnable()
+    {
+        if (playerHealth != null)
+            playerHealth.OnDamaged += HandleDamage;
+    }
+
+    private void OnDisable()
+    {
+        if (playerHealth != null)
+            playerHealth.OnDamaged -= HandleDamage;
+    }
+
+    private void HandleDamage(int currentHealth)
+    {
+        Flash();
+    }
+
+    public void Flash()
+    {
+        StopAllCoroutines();
+        StartCoroutine(FlashRoutine());
+    }
+
+    private IEnumerator FlashRoutine()
+    {
+        for (int i = 0; i < flashCount; i++)
+        {
+            spriteRenderer.material.color = flashColor;
+            yield return new WaitForSeconds(flashDuration);
+            spriteRenderer.material.color = originalColor;
+            yield return new WaitForSeconds(flashDuration);
+        }
+    }
+
 }
