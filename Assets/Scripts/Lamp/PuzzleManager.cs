0af257d3656455ab217ab4fc3cd18b4fb423bb98
TWITexe <pkotelnikov2000@gmail.com>
2025-10-24 01:09:31 +0700

    update(lamp puzzle) update puzzle mechanic

--- Assets/Scripts/Lamp/PuzzleManager.cs
+++ Assets/Scripts/Lamp/PuzzleManager.cs
@@ -9,10 +9,10 @@ public class PuzzleManager : MonoBehaviour
     [SerializeField]
     private int[][] leverLampIndices =
     {
-        new int[] {0, 1, 2, 3, 6},   
-        new int[] {2, 4, 5, 7, 8},      
-        new int[] {1, 3, 4, 6},               
-        new int[] {4, 6, 7}
+        new int[] {1, 4, 6, 7},   
+        new int[] {3, 4, 6, 9},      
+        new int[] {1, 3, 5, 7, 9},               
+        new int[] {2, 4, 5, 6, 8}
     };
 
     public void ActivateLever(int leverIndex)
