154df00445137c37df7194772272fd46571b240c
TWITexe <pkotelnikov2000@gmail.com>
2025-11-02 22:45:42 +0700

    update(script) add new button-triggers for painful logic

--- Assets/Scripts/Player/PainfulActions.cs
+++ Assets/Scripts/Player/PainfulActions.cs
@@ -21,7 +21,7 @@ public class PainfulActions : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.Space))
             playerHealth.TakeDamage(5);
 
     }
